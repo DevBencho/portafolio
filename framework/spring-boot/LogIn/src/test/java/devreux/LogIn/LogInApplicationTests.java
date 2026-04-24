f5f488473d29235e82d112f5e60a9fee9604d82b
DevBencho <tubenquintero023@gmail.com>
2026-04-24 13:07:37 -0400

    project created, all dependencies active, readey for next step

---
+++ framework/spring-boot/LogIn/src/test/java/devreux/LogIn/LogInApplicationTests.java
@@ -0,0 +1,13 @@
+package devreux.LogIn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
